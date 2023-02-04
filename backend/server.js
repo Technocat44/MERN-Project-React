7254ddd58a2a82acfd1747d414f50479e89cf49e
jamesaqu <jamesaqu@buffalo.edu>
2023-02-04 17:45:49 -0500

    added middleware, routers, controller

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8000;
-
+const { errorHandler } = require('./middleware/errorMiddleware')
+ 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome to the support desk API"})
 })
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(
     PORT,
