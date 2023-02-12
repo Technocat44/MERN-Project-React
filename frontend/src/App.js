972e4dfb78c6090bfd0a6382c7f2ac95a8b5ced4
jamesaqu <jamesaqu@buffalo.edu>
2023-02-12 11:56:02 -0500

    created three components and exported them from index.js and set up App.js with those components and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import { Home, Login, Register } from './pages';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
 function App() {
   return (
-    <div>
+    <>
+    <Router>
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
       My app 
-    </div>
+    </>
   );
 }
 
